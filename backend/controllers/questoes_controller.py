66786e7c4100d756d5ee339ed435888c9eaa8332
CAndrey6202 <dedey.tdf@gmail.com>
2026-08-21 15:03:39 -0300

    revert: desfaz permissao global de visualizacao para super admins no painel de envio

--- backend/controllers/questoes_controller.py
+++ backend/controllers/questoes_controller.py
@@ -376,14 +376,9 @@ def tela_envio():
         ).distinct().all()
         
     materias_vinculadas = set(vinculos_db)
-    is_admin_poderoso = current_user.role == 'super_admin' or current_user.is_admin_escola_in_school(escola_id)
 
     opcoes_autorizadas = []
     for opcao in opcoes_abertas:
-        if is_admin_poderoso:
-            opcoes_autorizadas.append(opcao)
-            continue
-            
         tem_vinculo = False
         for v_materia, v_edicao in materias_vinculadas:
             if v_materia == opcao["materia"]:
